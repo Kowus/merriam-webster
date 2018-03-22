2f4a7328bb18c51957de0be40d41596f9e7f2637
Kowus <barnabas@elite-education.org>
2018-03-22 17:03:16 +0000

    define method created

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 "use strict";
 
+const request = require("request");
 class MerriamWebster {
     constructor(config) {
         this.key1 = config.key1;
         this.key2 = config.key2;
     }
 
-    define(word) {
-        console.log(`DEFINITION of ${word}`);
+    define(word, done) {
+        let url = `https://www.dictionaryapi.com/api/v1/references/collegiate/xml/${word}?key=${this.key1}`
+        request(
+            {
+                url: url
+            },
+            (err, response, body) => {
+                if (err) {
+                    return done(err);
+                }
+                return done(null, body);
+            }
+        );
     }
 }
 
